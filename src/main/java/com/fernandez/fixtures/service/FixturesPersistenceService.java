19c1b664121da7a034519b02289c637293595de9
Juan Francisco Fernandez Herreros <31112111+juanfranciscofernandezherreros@users.noreply.github.com>
2026-09-26 18:05:51 +0200

    KAN-121 use atomic fixtures upsert

--- src/main/java/com/fernandez/fixtures/service/FixturesPersistenceService.java
+++ src/main/java/com/fernandez/fixtures/service/FixturesPersistenceService.java
@@ -2,22 +2,22 @@ package com.fernandez.fixtures.service;
 
 import com.fernandez.fixtures.avro.FixtureValue;
 import com.fernandez.fixtures.mapper.FixtureMapper;
-import com.fernandez.fixtures.repository.FixturesRepository;
+import com.fernandez.fixtures.repository.FixturesUpsertRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class FixturesPersistenceService {
-    private final FixturesRepository fixtures;
+    private final FixturesUpsertRepository fixtures;
     private final FixtureMapper mapper;
 
-    public FixturesPersistenceService(FixturesRepository fixtures, FixtureMapper mapper) {
+    public FixturesPersistenceService(FixturesUpsertRepository fixtures, FixtureMapper mapper) {
         this.fixtures = fixtures;
         this.mapper = mapper;
     }
 
     @Transactional
     public void persist(FixtureValue value) {
-        fixtures.saveAndFlush(mapper.toEntity(value));
+        fixtures.upsert(mapper.toEntity(value));
     }
 }
